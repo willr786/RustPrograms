e251c5622503c50aedad3037d02c518452c06019
willr786 <willr_786@yahoo.com>
2021-09-09 14:07:28 -0400

    Cargo Files

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\wr1276\Desktop\Will's Folder\Rust\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\wr1276\Desktop\Will's Folder\Rust\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Users\wr1276\Desktop\Will's\ Folder\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Users\wr1276\Desktop\Will's\ Folder\Rust\hello_cargo\src\main.rs
